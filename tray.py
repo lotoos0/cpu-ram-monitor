523aa3aa96c4caacb9cb3d62eb9045863e3bbfbf
lotoos0 <andii4444@gmail.com>
2024-09-30 21:14:51 +0200

    doc: added some comments

--- tray.py
+++ tray.py
@@ -2,7 +2,7 @@ from pystray import Icon, MenuItem, Menu
 from PIL import Image, ImageDraw
 from config_manager import load_config, reset_to_default
 from utils import night_mode, auto_night_mode, toggle_night_mode, toggle_auto_night_mode
-from gui import show_settings_dialog  # Import funkcji wyświetlającej ustawienia
+from gui import show_settings_dialog  # Import of a function displaying settings
 
 def create_image(color="green"):
     image = Image.new('RGB', (64, 64), color=(0, 0, 0))
@@ -15,10 +15,9 @@ def setup_tray(root):
         MenuItem('Night mode (manual)', lambda icon, item: toggle_night_mode(), checked=lambda item: night_mode),
         MenuItem('Automatic night mode', lambda icon, item: toggle_auto_night_mode(), checked=lambda item: auto_night_mode),
         MenuItem('Settings', lambda: root.after(0, show_settings_dialog)),
-        MenuItem('Reset to Default', lambda: root.after(0, lambda: reset_to_default(load_config()))),  # Załaduj konfigurację i zresetuj ustawienia z potwierdzeniem
+        MenuItem('Reset to Default', lambda: root.after(0, lambda: reset_to_default(load_config()))),  # Load configuration and reset settings with confirmation
         MenuItem('Quit', lambda icon, item: root.quit())
     )
     icon = Icon("CPU_RAM Monitor", create_image(), menu=menu)
     icon.run()
-
 
