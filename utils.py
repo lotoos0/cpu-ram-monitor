f9f512b36747ca1314f4edb2afc5aef3709db6aa
lotoos0 <andii4444@gmail.com>
2024-10-02 21:11:13 +0200

    move get_config_dir function from config_manager.py

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 from datetime import datetime
+import os
+from pathlib import Path
+
 
 night_mode = False
 auto_night_mode = False
@@ -21,3 +24,18 @@ def toggle_night_mode():
 def toggle_auto_night_mode():
     global auto_night_mode
     auto_night_mode = not auto_night_mode
+
+def get_config_dir():
+    """
+    Get or create the configuration directory in the user's Documents folder.
+    On Windows, it uses the Documents folder inside USERPROFILE.
+    On Linux, it uses the ~/Documents/CPU_RAM_Monitor folder.
+    """
+    if os.name == 'nt':  # Windows
+        config_dir = Path(os.getenv('USERPROFILE')) / 'Documents' / 'CPU_RAM_Monitor'
+    else:  # Linux and others
+        config_dir = Path.home() / 'Documents' / 'CPU_RAM_Monitor'
+
+    # Create the directory if it doesn't exist
+    config_dir.mkdir(parents=True, exist_ok=True)
+    return config_dir
